c2cd2f7be1b33a6bccabc950e97de66630a657b4
Amit Sain <amit2976297@gmail.com>
2025-06-16 15:05:04 +0530

    new codes added

---
+++ QueueLL.java
@@ -0,0 +1,95 @@
+public class QueueLL {
+    static class Node {
+
+        int data;
+        Node next;
+
+        public Node(int data) {
+
+            this.data = data;
+            this.next = null;
+
+        }
+    }
+
+    static class Queue{
+
+        static Node head = null;
+        static Node tail = null;
+
+        // Empty Queue 
+
+        public static boolean isEmpty() {
+            return head == null && tail == null;
+        }
+
+        // Add 
+
+        public static void add(int data) {
+
+            Node newnode = new Node(data);
+
+            // Adding at first
+
+            if ( head == null ) {
+                head = tail = newnode;
+                return;
+            } 
+
+            tail.next = newnode;
+            tail = newnode;
+        }
+
+        // Remove 
+
+        public static int remove() {
+
+            if ( isEmpty() ) {
+                System.out.println("Queue is already empty");
+                return -1; 
+            }
+
+            int front = head.data;
+
+            // Single element 
+
+            if ( tail == head ) {
+
+                tail = head = null;
+
+            } else {
+                head = head.next ;
+            }
+             
+            return front;
+        }
+
+        // PEEK () 
+
+        public static int peek() {
+
+            if ( isEmpty() ) {
+                System.out.println("Queue is already empty");
+                return -1; 
+            }
+
+            return head.data;
+
+        }
+    }
+
+    public static void main ( String args[] ) {
+
+        Queue q = new Queue();
+
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        while ( !q.isEmpty() ) {
+            System.out.println(q.peek());
+            q.remove();
+        }
+
+    }
+}
