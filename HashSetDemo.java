cfc887d0b6a2e3e255ef073e6fee5ec9115afcb6
Amit Sain <amit2976297@gmail.com>
2025-07-09 11:20:04 +0530

    HashSet Codes and Question code added

---
+++ HashSetDemo.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+
+public class HashSetDemo {
+
+    public static void main ( String args[] ) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        set.add(1);
+        set.add(4);
+        set.add(2);
+        set.add(2);
+        set.add(3);
+
+        System.out.println(set);
+
+        System.out.println(set.contains(2));
+
+        set.remove(2) ;
+
+        System.out.println(set);
+
+
+    }
+}
+    
---
+++ IterationInHashSet.java
@@ -0,0 +1,43 @@
+import java.util.* ;
+
+public class IterationInHashSet {
+
+    public static void main ( String args[] ) {
+
+        HashSet<String> cities = new HashSet<>() ;
+
+        cities.add("Bangalore") ;
+        cities.add("Noida") ;
+        cities.add("Gurugram") ;
+        cities.add("Delhi") ;
+        cities.add("Mumbai") ;
+
+        // using Iterators
+
+        System.out.println("Using Iteratior :");
+        System.out.println() ;
+
+        Iterator it = cities.iterator() ;
+
+        while ( it.hasNext() ) {
+
+            System.out.println( it.next() ) ;
+
+        }
+
+        System.out.println() ;
+
+        System.out.println("Using Advanced For Loop :");
+        System.out.println() ;
+
+        // Using advanced for loop 
+
+        for ( String city : cities ) {
+
+            System.out.println(city);
+        }
+
+
+    }
+    
+}
