dd7b86b5d70298c6effbf26258c8a5ba61f3a27e
Amit Sain <amit2976297@gmail.com>
2025-07-10 14:28:08 +0530

    New Question codes of Hashing added

---
+++ LargestSubarrayWithZeroSum.java
@@ -0,0 +1,28 @@
+import java.util.* ;
+
+public class LargestSubarrayWithZeroSum {
+
+    public static void main ( String args[] ) {
+
+        int arr[] = { 15, -2, 2, -8, 1, 7, 10, 23 } ;
+
+        HashMap < Integer , Integer > map = new HashMap<>();
+
+        int sum = 0 ;
+        int len = 0 ;
+
+        for ( int i = 0; i < arr.length ; i++ ) {
+
+            sum += arr[i] ;
+
+            if ( map.containsKey(sum) ) {
+                len = Math.max ( len , i - map.get( sum ) ) ;
+            } else {
+                map.put ( sum , len ) ;
+            }
+        }
+
+        System.out.println ( "Largest Subarray length with sum 0 => " + len ) ;
+    }
+    
+}
