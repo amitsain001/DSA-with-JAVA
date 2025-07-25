4f82ffab74ccc4882202fb4840c471a5fc11e2fe
Amit Sain <amit2976297@gmail.com>
2025-07-25 11:11:10 +0530

    Floodfill algo and kruskal's algo code added

--- FloodFillAlgo.java
+++ FloodFillAlgo.java
@@ -1,4 +1,4 @@
-// Problem no. 733 ( Leetcode )
+// Problem no. 733 ( Leetcode ) = Test this on leet code without main()
 
 public class FloodFillAlgo {
 
@@ -6,9 +6,11 @@ public class FloodFillAlgo {
 
         // Base Case 
 
-        if ( sr < 0 || sc < 0 || visit[sr][sc] || sr >= image.length || sc >= image[0].length || image[sr][sc] != orgclr  ) {
+        if ( sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length || visit[sr][sc] || image[sr][sc] != orgclr  ) {
             return ;
         }
+
+        visit[sr][sc] = true ;
 
         image[sr][sc] = color ;
 
@@ -29,15 +31,10 @@ public class FloodFillAlgo {
         helper(image, sr+1, sc, color, visit, orgclr);
     }
 
-    public int[][] floodfill ( int image[][] , int sr , int sc , int color ) {
+    public int[][] floodFill ( int image[][] , int sr , int sc , int color ) {
 
         boolean visit[][] = new boolean[image.length][image[0].length] ;
         helper(image , sr , sc , color , visit , image[sr][sc] ) ;
         return image ;
     }
-
-    public static void main ( String args[] ) {
-
-    }
-    
 }
