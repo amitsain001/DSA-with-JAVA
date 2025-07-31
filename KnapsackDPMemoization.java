291effed28eafa585da01646ea104235172173e0
Amit Sain <amit2976297@gmail.com>
2025-07-31 13:06:10 +0530

    0-1 Knapsack using memoisation code

---
+++ KnapsackDPMemoization.java
@@ -0,0 +1,55 @@
+public class KnapsackDPMemoization {
+
+    public static int recursiveknapsackmemo ( int val[] , int wt[] , int W , int n , int dp[][]) {
+
+        // Base case 
+
+        if ( W == 0 || n == 0 ) {
+            return 0 ;
+        }
+
+        if ( dp[n][W] != -1 ) {
+            return dp[n][W] ;
+        }
+
+        if ( wt[n-1] <= W ) {
+
+            //include 
+
+            int ans1 = val[n-1] + recursiveknapsackmemo(val, wt, W-wt[n-1], n-1 , dp) ;
+
+            //Exclude 
+
+            int ans2 = recursiveknapsackmemo(val, wt, W, n-1 , dp) ;
+
+            dp[n][W] =  Math.max ( ans1 , ans2 ) ;
+            return dp[n][W] ;
+
+        } else {
+
+            // Exclude 
+            dp[n][W] = recursiveknapsackmemo( val, wt, W, n-1 , dp ) ;
+            return dp[n][W] ;
+        }
+
+    }
+    
+    public static void main ( String args[] ) {
+
+        int val[] = { 15 , 14 , 10 , 45 , 30  } ;
+        int wt[] = { 2 , 5 , 1 , 3 , 4 } ;
+        int W = 7 ;
+
+        int dp[][] = new int[val.length+1][W+1] ;
+
+        for ( int i = 0 ; i < dp.length ; i++ ) {
+            for ( int j = 0 ; j < dp[0].length ; j++ ) {
+                dp[i][j] = -1 ;
+            }
+        }
+
+        System.out.println ( recursiveknapsackmemo(val, wt, W, val.length , dp ) ) ;
+
+    }
+    
+}
