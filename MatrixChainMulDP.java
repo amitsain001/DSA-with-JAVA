7e541474cfebed136ce4ba2da8de50ab07065462
Amit Sain <amit2976297@gmail.com>
2025-08-16 13:18:09 +0530

    Matrix Chain Multiplication Code

---
+++ MatrixChainMulDP.java
@@ -0,0 +1,106 @@
+import java.util.Arrays;
+
+public class MatrixChainMulDP {
+
+    // Recursion Approach
+
+    public static int mincostrec ( int arr[] , int i , int  j ) {
+
+        if ( i == j ) {
+            return 0 ;
+        }
+
+        int ans  = Integer.MAX_VALUE ;
+
+        for ( int  k = i ; k < j ; k++ ) {
+
+            int cost1 = mincostrec(arr, i, k) ;
+            int cost2 = mincostrec(arr, k+1, j) ;
+            int cost3 = arr[i-1] * arr[k] * arr[j] ;
+            int finalcost = cost1 + cost2 + cost3 ;
+            ans = Math.min(ans, finalcost) ; 
+        }
+
+        return ans ;
+    }
+
+    // Memoization Approach
+
+    public static int mincostmemo ( int arr[] , int i , int  j , int dp[][] ) {
+
+        if ( i == j ) {
+            return 0 ;
+        }
+
+        if ( dp[i][j] != -1 ) {
+            return dp[i][j] ;
+        }
+
+        int ans  = Integer.MAX_VALUE ;
+
+        for ( int  k = i ; k < j ; k++ ) {
+
+            int cost1 = mincostmemo(arr, i, k , dp) ;
+            int cost2 = mincostmemo(arr, k+1, j , dp) ;
+            int cost3 = arr[i-1] * arr[k] * arr[j] ;
+            int finalcost = cost1 + cost2 + cost3 ;
+            ans = Math.min(ans, finalcost) ; 
+        }
+
+        return dp[i][j] = ans ;
+    }
+
+    // Tabulation Approach 
+
+    public static int mincosttabu ( int arr[] ) {
+
+        int n = arr.length ;
+
+        int dp[][] = new int[n][n] ;
+
+        for ( int i = 0 ; i < n ; i++ ) {
+            dp[i][i] = 0 ;
+        }
+
+        // Initialization
+
+        for ( int len = 2 ; len <= n-1 ; len++ ) {
+
+            for ( int i = 1 ; i <= n-len ; i++ ) {
+
+                int j = i + len - 1 ;
+                dp[i][j] = Integer.MAX_VALUE ;
+
+                for ( int k = i ; k < j ; k++ ) {
+
+                    int cost1 = dp[i][k] ;
+                    int cost2 = dp[k+1][j] ;
+                    int cost3 = arr[i-1] * arr[k] * arr[j] ;
+
+                    dp[i][j] = Math.min ( dp[i][j] , cost1+cost2+cost3 ) ;
+
+                }
+            }
+        }
+
+        return dp[1][n-1] ;
+    }
+
+    public static void main ( String args[] ) {
+
+        int arr[] = { 1 , 2 , 3 , 4 , 3 } ;
+        int n = arr.length ;
+
+        int dp[][] = new int[n][n] ;
+        for ( int  i = 0 ; i < n ; i++ ) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        System.out.println ("Minimum cost using recursion approach : " + mincostrec(arr, 1, n-1 ) ) ;
+
+        System.out.println ("Minimum cost using Memoization approach : " + mincostmemo(arr, 1, n-1 , dp) ) ;
+
+        System.out.println ("Minimum cost using Tabulation approach : " + mincosttabu(arr) ) ;
+    }
+    
+}
